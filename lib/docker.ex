14e856fce22a4c567b6d73f86c8ba05f20dadd7a
Yeboster <yeboster@gmail.com>
2019-10-27 10:19:55 +0100

    Scrape docker stat command.

--- lib/docker.ex
+++ lib/docker.ex
@@ -34,17 +34,15 @@ defmodule Docker do
   Scrape the input as the docker ps command output
   """
   def scrape_ps(output) when is_bitstring(output) do
-    str_not_empty? = fn str when is_bitstring(str) -> str != "" end
-
     lines =
       String.split(output, "\n")
       |> List.delete_at(0)
-      |> Enum.filter(str_not_empty?)
+      |> Enum.filter(&str_not_empty?/1)
 
     # Using stream since it's lazy
     Stream.map(lines, fn line ->
       String.split(line, "  ")
-      |> Enum.filter(str_not_empty?)
+      |> Enum.filter(&str_not_empty?/1)
     end)
     |> Enum.map(&scrape_ps/1)
   end
@@ -59,7 +57,36 @@ defmodule Docker do
     scrape_ps(output)
   end
 
+  @doc """
+  Scrape docker stat row into a map type
+  """
+  def scrape_stats(values) when is_list(values) and length(values) == 8 do
+    stats_keys = [:id, :name, :cpu, :memory, :memory_percentage, :net_io, :block_io, :pids]
+
+    lists_into_map(stats_keys, values)
+  end
+
+  @doc """
+  Scrape docker stat command output into a list containing map type
+  """
+  def scrape_stats(output) when is_bitstring(output) do
+    String.split(output, "\n")
+    |> List.delete_at(0)
+    |> Stream.filter(&str_not_empty?/1)
+    |> Stream.map(fn line ->
+      String.split(line, "  ")
+      |> Enum.filter(&str_not_empty?/1)
+    end)
+    |> Enum.map(&scrape_stats/1)
+  end
+
+  @doc """
+  Scrape docker stat command calling it into the shell
+  """
   def scrape_stats() do
+    {output, 0} = cmd_docker(["stats", "--no-stream"])
+
+    scrape_stats(output)
   end
 
   def scrape_ip_addr(container_id) do
@@ -68,7 +95,11 @@ defmodule Docker do
   @spec lists_into_map([String.t()], [String.t()]) :: [%{key: String.t()}]
   defp lists_into_map(keys, values) when is_list(keys) and is_list(values) do
     Stream.zip(keys, values)
-    |> Enum.into(%{}, fn {key, value} -> {key, value} end)
+    |> Enum.into(%{}, fn {key, value} -> {key, String.trim(value)} end)
+  end
+
+  defp str_not_empty?(str) when is_bitstring(str) do
+    str != ""
   end
 
   defp cmd_docker(commands) when is_list(commands) do
