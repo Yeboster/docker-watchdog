e516537a4cc9ab78205aefce96c6b23c72e2669e
Yeboster <yeboster@gmail.com>
2019-10-28 15:03:00 +0100

    Added preliminar container db migration.

---
+++ priv/repo/migrations/20191028090559_create_containers.exs
@@ -0,0 +1,15 @@
+defmodule Database.Repo.Migrations.CreateContainers do
+  use Ecto.Migration
+
+  def change do
+    create table(:container) do
+      add :container_id, :string
+      add :image, :string
+      add :command, :string
+      add :name, :string
+      add :container_created_at, :string
+      add :port, :string
+      add :status, :string
+    end
+  end
+end
