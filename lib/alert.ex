0a89ec28ea759eea9b884057efa8c9b28df996b7
Yeboster <yeboster@gmail.com>
2019-11-07 18:39:53 +0100

    Added alert module (2/2).

--- lib/alert.ex
+++ lib/alert.ex
@@ -1,9 +1,16 @@
 defmodule Docker.Alert do
+  import Docker.Alert.Utils
 
   defguard is_container_map(map) when is_map(map)
 
   def inform_of(map) when is_container_map(map) do
-    # TODO: alert telegram channel
+    message =
+      "The container named"
+      |> with_bold(map.name)
+      |> with_text("exited with")
+      |> with_bold(map.status)
+      |> with_text("status")
+
+    send_markdown(Application.get_env(:watchdog_bot, :telegram_channel_id), message)
   end
-  
 end
