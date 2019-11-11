28764ed5b7f33c9fb782082a5e90d78d81f62412
Yeboster <yeboster@gmail.com>
2019-11-11 01:22:55 +0100

    Added username check.

--- lib/watchdog_bot/bot.ex
+++ lib/watchdog_bot/bot.ex
@@ -8,7 +8,22 @@ defmodule WatchdogBot.Bot do
 
   def bot(), do: @bot
 
-  def handle({:command, "start", _msg}, context) do
-    answer(context, "Hi!")
+  def handle({command, str, msg}, context) do
+    if allowed?(msg) do
+      authenticated({command, str, msg}, context)
+    else
+      answer(context, "Sir #{msg[:from][:first_name]}, you are not allowed to use this bot.")
+    end
+  end
+
+  def allowed?(msg) do
+    ids_allowed = [32_234_185]
+    current_id = msg[:from][:id]
+
+    Enum.any?(ids_allowed, &(&1 == current_id))
+  end
+
+  def authenticated({:command, "start", _msg}, context) do
+    answer(context, "Conquer the world!")
   end
 end
