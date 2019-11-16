5bc4202557c6dc648b1a66e0911e1cc52d589e11
Yeboster <yeboster@gmail.com>
2019-11-16 22:24:17 +0100

    Remove debug logs.

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -13,4 +13,5 @@ config :watchdog_bot, Docker.Repo,
   port: 5432
 
 config :logger, :console,
+  level: :info,
   format: "{\"timestamp\":\"$time\", \"level\":\"$level\", \"message\":\"$message\"}\n"
